d6441573f72bff0235f671ea95a1959b6bd233bd
franktee <could.net@gmail.com>
2021-12-19 20:15:24 +0800

    trim

---
+++ trim/widget.cpp
@@ -0,0 +1,28 @@
+ #include <QClipboard>
+#include "widget.h"
+#include "./ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    QObject::connect(ui->paste, &QPushButton::clicked, [this](){
+        QClipboard *clipboard = QGuiApplication::clipboard();
+        ui->plainTextEdit->setPlainText(clipboard->text());
+    });
+    QObject::connect(ui->trim, &QPushButton::clicked, [this](){
+        qDebug() << ui->plainTextEdit->toPlainText() << "\n";
+        ui->plainTextEdit->setPlainText(ui->plainTextEdit->toPlainText().replace("\n", " "));
+    });
+    QObject::connect(ui->copy, &QPushButton::clicked, [this](){
+        QClipboard *clipboard = QGuiApplication::clipboard();
+        clipboard->setText(ui->plainTextEdit->toPlainText());
+    });
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
