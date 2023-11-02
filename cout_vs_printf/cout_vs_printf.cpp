9b13edc3b3a24da71c814a4c36112bb5f043390f
frank <could.net@gmail.com>
2023-11-02 17:24:47 +0800

    added fmt

--- cout_vs_printf/cout_vs_printf.cpp
+++ cout_vs_printf/cout_vs_printf.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <format>
 #include <print>
+#include <fmt/printf.h>
 
 void test_printf_string(benchmark::State& state) {
     for(auto _: state) {
@@ -22,6 +23,13 @@ void test_print_string(benchmark::State& state) {
     std::cout.sync_with_stdio(false);
     for(auto _: state) {
         std::println("hello world");
+    }
+}
+
+void test_fmt_print_string(benchmark::State& state) {
+    std::cout.sync_with_stdio(false);
+    for(auto _: state) {
+        fmt::println("hello world");
     }
 }
 
@@ -47,11 +55,20 @@ void test_print_int(benchmark::State& state) {
     }
 }
 
+void test_fmt_pint_int(benchmark::State& state) {
+    int i = 0;
+    for(auto _: state) {
+        fmt::println("{}", ++i);
+    }
+}
+
 BENCHMARK(test_printf_string);
 BENCHMARK(test_cout_string);
 BENCHMARK(test_print_string);
+BENCHMARK(test_fmt_print_string);
 BENCHMARK(test_printf_int);
 BENCHMARK(test_cout_int);
 BENCHMARK(test_print_int);
+BENCHMARK(test_fmt_pint_int);
 
 BENCHMARK_MAIN();
