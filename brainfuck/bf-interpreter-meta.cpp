4664304e45048838b0e508432438c66758737f91
franktea <could.net@gmail.com>
2024-05-14 10:23:39 +0800

    hello world of brainfuck

--- brainfuck/bf-interpreter-meta.cpp
+++ brainfuck/bf-interpreter-meta.cpp
@@ -1,74 +1,49 @@
-#include<iostream>
-
-// 定义Brainfuck操作
-enum BrainfuckOp {
-    INC_PTR,
-    DEC_PTR,
-    INC_VAL,
-    DEC_VAL,
-    OUTPUT,
-    INPUT,
-    JUMP_FORWARD,
-    JUMP_BACKWARD
-};
-
-// 将字符转换为Brainfuck操作
-constexpr BrainfuckOp charToOp(char c) {
-    switch (c) {
-        case '>': return INC_PTR;
-        case '<': return DEC_PTR;
-        case '+': return INC_VAL;
-        case '-': return DEC_VAL;
-        case '.': return OUTPUT;
-        case ',': return INPUT;
-        case '[': return JUMP_FORWARD;
-        case ']': return JUMP_BACKWARD;
-        default: return static_cast<BrainfuckOp>(-1);
-    }
-}
+#include <iostream>
+#include <array>
 
 // 解释器模板
-template<BrainfuckOp... ops>
+template<size_t N>
 struct BrainfuckInterpreter {
     static constexpr int memorySize = 30000;
-    static constexpr int stackSize = 1000;
+    const std::array<char, N> ops;
 
-    static void run() {
+    constexpr explicit BrainfuckInterpreter(const std::array<char, N> str) :
+        ops(str) {}
+
+    constexpr void run() const {
         char memory[memorySize] = {0};
         char* ptr = memory;
-        int stack[stackSize] = {0};
-        int sp = 0;
 
         int ip = 0;
-        while (ip< sizeof...(ops)) {
+        while (ip< N) {
             switch (ops[ip]) {
-                case INC_PTR: ++ptr; break;
-                case DEC_PTR: --ptr; break;
-                case INC_VAL: ++*ptr; break;
-                case DEC_VAL: --*ptr; break;
-                case OUTPUT: std::cout << *ptr; break;
-                case INPUT: std::cin >> *ptr; break;
-                case JUMP_FORWARD:
+                case '>': ++ptr; break;
+                case '<': --ptr; break;
+                case '+': ++*ptr; break;
+                case '-': --*ptr; break;
+                case '.': std::cout.put(*ptr); break;
+                case ',': std::cin >> *ptr; break;
+                case '[':
                     if (*ptr == 0) {
                         int depth = 1;
                         while (depth > 0) {
                             ++ip;
-                            if (ops[ip] == JUMP_FORWARD) {
+                            if (ops[ip] == '[') {
                                 ++depth;
-                            } else if (ops[ip] == JUMP_BACKWARD) {
+                            } else if (ops[ip] == ']') {
                                 --depth;
                             }
                         }
                     }
                     break;
-                case JUMP_BACKWARD:
+                case ']':
                     if (*ptr != 0) {
                         int depth = 1;
                         while (depth > 0) {
                             --ip;
-                            if (ops[ip] == JUMP_FORWARD) {
+                            if (ops[ip] == '[') {
                                 --depth;
-                            } else if (ops[ip] == JUMP_BACKWARD) {
+                            } else if (ops[ip] == ']') {
                                 ++depth;
                             }
                         }
@@ -80,14 +55,17 @@ struct BrainfuckInterpreter {
     }
 };
 
-// 将字符串转换为Brainfuck解释器模板
 template <size_t N>
 constexpr auto makeBrainfuckInterpreter(const char (&str)[N]) {
-    return BrainfuckInterpreter<charToOp(str[0]), charToOp(str[1]), charToOp(str[2]), charToOp(str[3]), charToOp(str[4]), charToOp(str[5]), charToOp(str[6]), charToOp(str[7]), charToOp(str[8]), charToOp(str[9])...>();
+    std::array<char, N> ops;
+    for(size_t i = 0; i < N; ++i) {
+        ops[i] = str[i];
+    }
+    return BrainfuckInterpreter<N>(ops);
 }
 
 int main() {
-    constexpr auto interpreter = makeBrainfuckInterpreter("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.<+++++++.--------.+++.------.--------.>+.>++.");
+    constexpr auto interpreter = makeBrainfuckInterpreter(">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.");
     interpreter.run();
     return 0;
 }
