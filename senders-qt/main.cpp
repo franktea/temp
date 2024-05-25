02e2ddf8eea99dd49633691f32cf16831f1f2f18
franktea <could.net@gmail.com>
2024-05-25 15:04:05 +0800

    get file length

--- senders-qt/main.cpp
+++ senders-qt/main.cpp
@@ -1,11 +1,27 @@
 #include <QCoreApplication>
 #include <QTimer>
 #include <iostream>
+#include <QObject>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QNetworkAccessManager>
+#include <optional>
 #include "qtstdexec.h"
 #include "exec/task.hpp"
 #include "exec/async_scope.hpp"
 
 using namespace QtSteExec;
+
+exec::task<void> FetchFileLength() {
+    std::cout<<"start fetch file...\n";
+    QNetworkAccessManager nam;
+    QNetworkRequest req = QNetworkRequest(QUrl(QLatin1String("https://ftp.funet.fi/pub/Linux/kernel/v5.x/linux-5.19.tar.gz")));
+    QNetworkReply* reply = nam.head(req);
+    co_await QObjectAsSender(reply, &QNetworkReply::finished);
+    QVariant length = reply->header(QNetworkRequest::ContentLengthHeader);
+    std::cout<<"get file info length = "<< length.value<long>() << "\n";
+    reply->deleteLater();
+}
 
 exec::task<void> AsyncSleep() {
     QTimer timer;
@@ -21,6 +37,8 @@ int main(int argc, char *argv[]) {
     exec::async_scope scope;
     auto t = AsyncSleep();
     scope.spawn(std::move(t));
+    auto l = FetchFileLength();
+    scope.spawn(std::move(l));
     std::cout<<"start running...\n";
 
     return a.exec();
--- senders-qt/qtstdexec.h
+++ senders-qt/qtstdexec.h
@@ -119,10 +119,17 @@ public:
 
     QObjectSender(QObj* obj, m_ptr_type ptr) : obj_(obj), m_ptr_(ptr) {}
 
-    STDEXEC_MEMFN_DECL(auto connect)(this auto sender, stdexec::receiver auto&& receiver) {
+    /*STDEXEC_MEMFN_DECL(auto connect)(this auto sender, stdexec::receiver auto&& receiver) {
     //auto connect(this auto sender, stdexec::receiver auto&& receiver) {
         return QObjectOperationState<std::remove_cvref_t<decltype(receiver)>, QObj, Ret, Args...>(std::move(receiver), sender.obj_, sender.m_ptr_);
+    }*/
+    template <class Recv>
+    friend inline QObjectOperationState<Recv, QObj, Ret, Args...>
+    tag_invoke(stdexec::tag_t<stdexec::connect>, QObjectSender sender, Recv&& receiver)
+    {
+        return QObjectOperationState<Recv, QObj, Ret, Args...>(std::move(receiver), sender.obj_, sender.m_ptr_);
     }
+
 private:
     QObj* obj_;
     m_ptr_type m_ptr_;
